2161081b1a4c3abdd654d0fb4ee3b38063480d4a
Davej66 <daijacobs@gmail.com>
2022-02-28 11:31:47 +0000

    Adding CSS modules with React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,14 @@
 import css from './App.module.css';
+import MyIllustrations from './components/MyIllustrations';
+import MyPaintings from './components/MyPaintings';
+import MyPhotos from './components/MyPhotos';
 
 function App() {
   return (
-    <div className={css.App}>
-      {/* Add your components here */}
+    <div className={css.sidebar}>
+      {<MyPhotos/> }
+      {<MyIllustrations/> }
+      {<MyPaintings/> }
     </div>
   );
 }
